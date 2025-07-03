5338cf8815d458b0942d383f53da5a50e11a3f98
Lucas Lima <lucaslimadevx@gmail.com>
2025-07-03 15:01:10 -0300

    Update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -41,6 +41,13 @@ public class Main {
 		
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("\n===== TEST 5: seller findAll =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		
+		System.out.println("Update completed");
 	}
 
 }
